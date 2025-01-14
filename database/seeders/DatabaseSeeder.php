0f0f4ab8fe324d4621906d35bd7c6a965d96517b
znaguiachraf <bangiachraf@gmail.com>
2025-01-14 14:47:15 +0100

    Add combined reminder permissions seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(DefaultDataUsersTableSeeder::class);
         $this->call([
-            AddReminderPermissionsToSuperAdmin::class
+            AddReminderPermissionsToSuperAdmin::class,
+            AllReminderPermissionsSeeder::class
         ]);
     }
 }
