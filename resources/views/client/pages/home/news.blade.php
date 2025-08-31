6a8d720d08158f6b57f0fa042d9305ffbe9d9c06
elbejauinada <151253495+elbejauinada@users.noreply.github.com>
2025-08-31 15:17:14 +0100

    home changes footer and instagram

--- resources/views/client/pages/home/news.blade.php
+++ resources/views/client/pages/home/news.blade.php
@@ -1,27 +1,32 @@
-<!-- News Section Start -->
+<!-- Instagram Posts Section Start -->
 <section class="news-section section-padding fix">
     <div class="container">
         <div class="section-title text-center">
-            <img src="{{ asset('assets/images/client/sub-icon.png') }}" alt="icon-img" class="wow fadeInUp">
-            <span class="wow fadeInUp" data-wow-delay=".2s">From the Blog</span>
-            <h2 class="wow fadeInUp" data-wow-delay=".4s">Latest News & <br> Articles From the Blog</h2>
+            <img src="/assets/images/client/instagram-icon.png" alt="">
+            <span class="wow fadeInUp" data-wow-delay=".2s">From Instagram</span>
+            <h2 class="wow fadeInUp" data-wow-delay=".4s">Latest <br> Instagram Posts</h2>
         </div>
         <div class="row">
             @foreach ([1, 2, 3] as $i)
                 <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay=".{{ 2 * $i + 1 }}s">
                     <div class="news-card-items">
-                        <div class="news-image">
-                            <img src="{{ asset('assets/images/client/news/0' . $i . '.jpg') }}" alt="news-img">
-                            <div class="post-date"></div>
+                        <div class="news-image relative">
+                            <img src="{{ asset('assets/images/client/insta/0' . $i . '.jpg') }}" alt="insta-post">
+                            <!-- Instagram icon overlay -->
+                            <a href="#" class="absolute bottom-3 right-3 bg-white rounded-full p-2 shadow">
+                                <img src="/assets/images/client/instagram-icon.png" alt="">
+                            </a>
                         </div>
                         <div class="news-content">
-                            <div class="post-client"><img src="assets/images/client/instagram-icon.png" alt="img">
+                            <div class="flex items-center gap-2 mb-2">
+                                <i class="far fa-heart text-gray-500"></i>
+                                <span>{{ rand(20, 150) }} likes</span>
+                                <i class="far fa-comment text-gray-500 ml-4"></i>
+                                <span>{{ rand(5, 30) }} comments</span>
                             </div>
-                            <div class="news-cont">
-                                <h5><a href="#">Sample Blog Post {{ $i }}</a></h5>
-                                <p>Brief description about blog post {{ $i }}.</p>
-                            </div>
-                            <ul></ul>
+                            <p>Sample caption for Instagram post {{ $i }} with hashtags #mock #design #demo
+                            </p>
+                            <a href="#" class=theme-btn bg-color w-100 text-center">View on Instagram</a>
                         </div>
                     </div>
                 </div>
--- resources/views/client/pages/home/testimonials.blade.php
+++ resources/views/client/pages/home/testimonials.blade.php
@@ -8,78 +8,87 @@
             <div class="section-title">
                 <img src="{{ asset('assets/images/client/sub-icon.png') }}" alt="icon-img" class="wow fadeInUp">
                 <span class="wow fadeInUp" data-wow-delay=".2s">our testimonials</span>
-                <h2 class="wow fadeInUp" data-wow-delay=".4s">What They're Talking <br> About Remons</h2>
+                <h2 class="wow fadeInUp" data-wow-delay=".4s">What They're Talking <br> About Direct Onderweg</h2>
             </div>
-            <p class="wow fadeInUp" data-wow-delay=".5s">Lorem ipsum dolor sit amet nsectetur cing elituspe ndisse suscipit sagitis leo sit.</p>
+            <p class="wow fadeInUp" data-wow-delay=".5s">
+                Hear from our valued clients who have experienced our commitment to quality and service.
+                Their stories reflect the trust, satisfaction, and long-lasting relationships we build every day at
+                Direct Onderweg.
+            </p>
         </div>
 
         <div class="swiper testimonial-slider">
             <div class="swiper-wrapper">
                 @php
-                $testimonials = [
-                    [
-                        'name' => 'Jessica Brown',
-                        'role' => 'Customer',
-                        'image' => 'client-1.png',
-                        'rating' => 4,
-                        'text' => 'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.'
-                    ],
-                    [
-                        'name' => 'Kevin Martin',
-                        'role' => 'Customer',
-                        'image' => 'client-2.png',
-                        'rating' => 5,
-                        'text' => 'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.'
-                    ],
-                    [
-                        'name' => 'Jessica Brown',
-                        'role' => 'Customer',
-                        'image' => 'client-3.png',
-                        'rating' => 5,
-                        'text' => 'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.'
-                    ],
-                    [
-                        'name' => 'Michael Johnson',
-                        'role' => 'Customer',
-                        'image' => 'client-4.png',
-                        'rating' => 4,
-                        'text' => 'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.'
-                    ]
-                ];
+                    $testimonials = [
+                        [
+                            'name' => 'Jessica Brown',
+                            'role' => 'Customer',
+                            'image' => 'client-1.png',
+                            'rating' => 4,
+                            'text' =>
+                                'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.',
+                        ],
+                        [
+                            'name' => 'Kevin Martin',
+                            'role' => 'Customer',
+                            'image' => 'client-2.png',
+                            'rating' => 5,
+                            'text' =>
+                                'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.',
+                        ],
+                        [
+                            'name' => 'Jessica Brown',
+                            'role' => 'Customer',
+                            'image' => 'client-3.png',
+                            'rating' => 5,
+                            'text' =>
+                                'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.',
+                        ],
+                        [
+                            'name' => 'Michael Johnson',
+                            'role' => 'Customer',
+                            'image' => 'client-4.png',
+                            'rating' => 4,
+                            'text' =>
+                                'I was very impressed by the remons service lorem ipsum is simply free text used by copy typing refreshing. Neque porro est qui dolorem ipsum quia.',
+                        ],
+                    ];
                 @endphp
 
                 @foreach ($testimonials as $testimonial)
-                <div class="swiper-slide">
-                    <div class="testimonial-card-items">
-                        <div class="quote-icon">
-                            <i class="fas fa-quote-left"></i>
-                        </div>
-
-                        <div class="testimonial-content">
-                            <p>{{ $testimonial['text'] }}</p>
-
-                            <div class="star-rating">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <= $testimonial['rating'])
-                                        <i class="fas fa-star"></i>
-                                    @else
-                                        <i class="far fa-star"></i>
-                                    @endif
-                                @endfor
+                    <div class="swiper-slide">
+                        <div class="testimonial-card-items">
+                            <div class="quote-icon">
+                                <i class="fas fa-quote-left"></i>
                             </div>
-                        </div>
 
-                        <div class="client-info-items d-flex align-items-center gap-3">
-                            <div class="client-thumb">
-                                <img src="{{ asset('assets/images/client/testimonial/' . $testimonial['image']) }}" alt="{{ $testimonial['name'] }}">
+                            <div class="testimonial-content">
+                                <p>{{ $testimonial['text'] }}</p>
+
+                                <div class="star-rating">
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        @if ($i <= $testimonial['rating'])
+                                            <i class="fas fa-star"></i>
+                                        @else
+                                            <i class="far fa-star"></i>
+                                        @endif
+                                    @endfor
+                                </div>
                             </div>
-                            <div class="client-content">
-                                <h5>{{ $testimonial['name'] }}</h5>
-                                <p>{{ $testimonial['role'] }}</p>
+
+                            <div class="client-info-items d-flex align-items-center gap-3">
+                                <div class="client-thumb">
+                                    <img src="{{ asset('assets/images/client/testimonial/' . $testimonial['image']) }}"
+                                        alt="{{ $testimonial['name'] }}">
+                                </div>
+                                <div class="client-content">
+                                    <h5>{{ $testimonial['name'] }}</h5>
+                                    <p>{{ $testimonial['role'] }}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
             </div>
 
